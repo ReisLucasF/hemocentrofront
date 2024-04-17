fb07a4287916f711d90e55d49957a0eacb3400f1
ReisLucasF <reislucasf@icloud.com>
2024-04-17 15:55:17 -0300

    Titulo sessões att

--- hemocentro/agenda.php
+++ hemocentro/agenda.php
@@ -65,6 +65,7 @@ function transformarEventos($dadosApi) {
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src='fullcalendar/packages/core/pt-br.global.js'></script>
     <meta charset="UTF-8" />
+    <title>Agenda</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- Inclusão do CSS do Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
--- login.php
+++ login.php
@@ -5,6 +5,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login</title>
     <!-- Inclusão do CSS do Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- Inclusão do JS do Bootstrap -->
