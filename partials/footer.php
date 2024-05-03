f74f44dc05e35047cf06619c09ac375f71fc9f03
ReisLucasF <reislucasf@icloud.com>
2024-05-03 11:36:14 -0300

    shrink para o footer

--- partials/footer.php
+++ partials/footer.php
@@ -2,6 +2,14 @@
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script> -->
 
+<style>
+    footer {
+    flex-shrink: 0;
+    background-color: #333;
+    color: #fff;
+    bottom: 0;
+}
+</style>
 <footer class="footer py-3 bg-dark fixed-bottom">
     <div class="container text-center">
         <span class="text-light">Copyright ©2022 Hemocentro. designed by Devos Alliance</span>
